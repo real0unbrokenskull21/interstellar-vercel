5d6854c69c3fa1287494122c655653cf6ebd8ebf
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-06 14:48:06 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://gointerstellar.app/bare/',
+    bare: 'http://172.93.151.179:8080/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
