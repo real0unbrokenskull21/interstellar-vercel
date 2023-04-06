1813f426f439dd2879e32f1b0b50c3752d92e183
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-06 16:46:01 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'http://172.93.151.179:8080/bare/',
+    bare: 'http://example.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
